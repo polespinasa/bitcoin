a5463455b0ff89fd8eaee2f1d5f2e43f98209fed
Pol Espinasa <pol.espinasa@uab.cat>
2026-04-14 18:29:23 +0200

    wallet: Move ImportDescriptor and ProcessDescriptorsImport to imports.cpp
    Extract ImportDescriptor() and ProcessDescriptorsImport() out of backup.cpp and move it into imports.cpp so other future interfaces
    can use them without needing to know about RPC code.

--- src/wallet/imports.h
+++ src/wallet/imports.h
@@ -18,6 +18,7 @@ struct ImportDescriptorResult {
         WALLET_UNLOCK_NEEDED,
         MISC_ERROR
     };
+
     bool success{false};
     FailureReason reason{FailureReason::NONE};
     std::string error;
@@ -44,6 +45,19 @@ struct ImportDescriptorRequest {
     std::optional<std::pair<int64_t,int64_t>> range;
     std::optional<int64_t> next_index;
 };
+
+ImportDescriptorResult ImportDescriptor(CWallet& wallet,
+    const std::string& descriptor,
+    bool active,
+    const std::optional<bool>& internal,
+    const std::optional<std::string>& label,
+    int64_t timestamp,
+    const std::optional<std::pair<int64_t, int64_t>>& range,
+    const std::optional<int64_t>& next_idx) EXCLUSIVE_LOCKS_REQUIRED(wallet.cs_wallet);
+
+std::vector<ImportDescriptorResult> ProcessDescriptorsImport(CWallet& wallet,
+    std::vector<ImportDescriptorRequest> requests);
+
 }
 
 #endif // BITCOIN_WALLET_IMPORTS_H
